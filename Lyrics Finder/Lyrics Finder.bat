bf0782f17070a6b9c0fe19f50395fc8462ae7868
Lola4528 <152510910+Lola4528@users.noreply.github.com>
2023-12-14 13:25:09 -0600

    modifyed

--- Lyrics Finder/Lyrics Finder.bat
+++ Lyrics Finder/Lyrics Finder.bat
@@ -1,13 +1,9 @@
 @echo off
-title Lyrics Finder
 
+    echo what lyrics do you want to find?
+    set /p input=
 
-@echo What song do you want to look for lyrics to?
-set /p input=
-
-if %input%=="Rod Wave - Boyz Don't Cry" (
-    for loop "/workspaces/lola4528/Lyrics Finder/Rod Wave - Boyz Don't Cry.txt" (
-        echo "/workspaces/lola4528/Lyrics Finder/Rod Wave - Boyz Don't Cry.txt"
-    )
-)
-pause
+    if %input%=="Rod Wave- Boyz Don't Cry" {
+        FOR /F "tokens=* delims=" %%x in (Rod Wave - Boyz Don't Cry.txt) DO echo %%x
+        
+    }
